3e49575f12cda7f9a86f66592e398f2961c1f686
Forrest Fleming <ffleming@gmail.com>
2015-10-15 21:07:05 -0700

    Allow rmdir .
    Simple function wrapper for rmdir that, if called with . (or . is in
    args), deletes the current dir and goes up a level (not in that order).
    Doesn't work, of course, if the directory isn't empty (because it's
    rmdir)

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,5 +1,18 @@
 # shell/functions.sh
 
+function rmdir {
+  SEP=$IFS[0,1]
+  DOT="$SEP\.$SEP"
+  RMDIR="$(whereis rmdir)"
+  if [ "$*" =~ $DOT -o "$*" == '.' ]; then
+    CURDIR="$(pwd)"
+    cd ..
+    $RMDIR $CURDIR
+  else
+    $RMDIR $*
+  fi
+}
+
 #------------------------------------------------------------------------------
 # ENHANCED MKDIR FUNCTION
 #------------------------------------------------------------------------------
