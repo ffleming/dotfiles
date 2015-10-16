20d9a167361c80b18a202677ede35ae8669dd91c
Forrest Fleming <ffleming@gmail.com>
2015-10-15 21:09:09 -0700

    Remove functions I don't use

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -12,27 +12,3 @@ function rmdir {
     $RMDIR $*
   fi
 }
-
-#------------------------------------------------------------------------------
-# ENHANCED MKDIR FUNCTION
-#------------------------------------------------------------------------------
-function nd {
-  mkdir "$*" && \cd "$*"
-}
-
-#------------------------------------------------------------------------------
-# HOMEBREW
-#------------------------------------------------------------------------------
-function brew_remove {
-  brew rm "$1"  # remove package and dependencies
-  brew rm $(join <(brew leaves) <(brew deps "$1"))
-}
-
-function update_homebrew {
-  echo brew update  && brew update
-  echo brew upgrade && brew upgrade
-  echo brew cleanup && brew cleanup
-  echo brew doctor  && brew doctor
-}
-
-
